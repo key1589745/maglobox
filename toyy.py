62e1e642287e12e6859b92df788d5098f035c9fa
key1589745 <74999004+key1589745@users.noreply.github.com>
2022-12-09 11:45:00 +0800

    Update toyy.py

--- toyy.py
+++ toyy.py
@@ -14,6 +14,30 @@ import json
 import time
 import muggle_ocr
 
+import urllib3
+import SSL
+
+
+class CustomHttpAdapter (requests.adapters.HTTPAdapter):
+    # "Transport adapter" that allows us to use custom ssl_context.
+
+    def __init__(self, ssl_context=None, **kwargs):
+        self.ssl_context = ssl_context
+        super().__init__(**kwargs)
+
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = urllib3.poolmanager.PoolManager(
+            num_pools=connections, maxsize=maxsize,
+            block=block, ssl_context=self.ssl_context)
+
+
+def get_legacy_session():
+    ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+    ctx.options |= 0x4  # OP_LEGACY_SERVER_CONNECT
+    session = requests.session()
+    session.mount('https://', CustomHttpAdapter(ctx))
+    return session
+
 
 url_login = "https://uis.fudan.edu.cn/authserver/login"
 url_dailyFudan = "https://zlapp.fudan.edu.cn/site/ncov/fudanDaily"
@@ -33,7 +57,7 @@ class Login_Fudan:
         self.data = {'username': self.username,
                      'password': self.pwd
                      }
-        self.session = requests.session()
+        self.session = get_legacy_session()
 
     def logIn(self):
         '''
